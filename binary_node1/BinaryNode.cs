aab55bf889431057f0185f3ee31702f70c06a809
lleonardogr <lleonardogr@gmail.com>
2025-03-13 23:07:55 -0300

    feat: Initialize binary_node1 project with basic binary tree implementation

---
+++ binary_node1/BinaryNode.cs
@@ -0,0 +1,26 @@
+namespace binary_node1;
+
+public class BinaryNode<T>
+{
+    public T Value { get; set; }
+    public BinaryNode<T>? Left { get; set; }
+    public BinaryNode<T>? Right { get; set; }
+
+    public BinaryNode(T value)
+    {
+        Value = value;
+        Left = null;
+        Right = null;
+    }
+
+    public void AddLeftChild(T value) => Left = new BinaryNode<T>(value);
+    public void AddRightChild(T value) => Right = new BinaryNode<T>(value);
+
+    public override string? ToString()
+    {
+        return $"{Value}: {(Left == null  ? "null" : Left.Value)} " +
+               $"{(Right == null  ? "null" : Right.Value)}";
+    }
+    
+    
+}
---
+++ binary_node1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using binary_node1;
+
+Console.WriteLine("Hello to binary app!");
+
+var test_node = new BinaryNode<string>("Root");
+test_node.AddLeftChild("A");
+test_node.AddRightChild("B");
+test_node.Left?.AddLeftChild("C");
+test_node.Left?.AddRightChild("D");
+test_node.Right?.AddRightChild("E");
+test_node.Right?.Right?.AddRightChild("F");
+
+Console.WriteLine(test_node.ToString());
+Console.WriteLine(test_node.Left?.ToString());
+Console.WriteLine(test_node.Right?.ToString());
+Console.WriteLine(test_node.Left?.Left?.ToString());
+Console.WriteLine(test_node.Left?.Right?.ToString());
+Console.WriteLine(test_node.Right?.Right?.ToString());
+Console.WriteLine(test_node.Right?.Right?.Right?.ToString());
+
+
